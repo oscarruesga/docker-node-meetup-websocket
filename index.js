265602c9bd9430b387192806ce2db5f9426c5265
Oscar Ruesga <oscar.ruesga@gmail.com>
2015-12-30 11:43:30 +0100

    Introduzco logica tutum

--- index.js
+++ index.js
@@ -1,26 +1,71 @@
- var forever = require('forever-monitor');
+ 	var forever = require('forever-monitor');
 
-var minimist = require('minimist')
+ 	var Tutum = require('./tutum');
 
-var args = minimist(process.argv.slice(2), {
-  string: 'stream',
-  string: 'zk',
-  default: { stream: 'RSVPS', zk: 'kafka:2181' },
-});
+	var minimist = require('minimist')
+
+	var args = minimist(process.argv.slice(2), {
+		string: ['stream', 'zk', 'tutum-user', 'tutum-key'],
+		default: { stream: 'RSVPS', zk: 'kafka:2181', tutum_user: 'oscarruesga', tutum_key: '5a33da87970a7b7c7044dce22217c3336b2072132ac8c71b' },
+	});
+	
+	var host = args.zk;
+
+	var child = new (forever.Monitor)('ws_rsvps.js', {
+		max: 5,
+		silent: false,
+		args: ['--stream', args.stream, '--zk', host],
+		'spinSleepTime': 5000,
+	});
+
+	if (args.tutum_user != '' && args.tutum_key!= '') {
+		var tutum = new Tutum({
+		  username: args.tutum_user,
+    	  apiKey: args.tutum_key
+		});
+
+		tutum.get('/container/', { name: 'kafka-1' }, function (err, res) {
+		  if (err) {
+		    throw err;
+		  }
+		  
+		  if (res.objects[0].private_ip) {
+		  	console.log (res.objects[0].private_ip);
+		  	host = res.objects[0].private_ip + ':2181';
+		  }
+		  launchApp ();
+		});
+
+
+	} else {
+		launchApp ();
+	}
+
+
+	function launchApp () {
+		var child = new (forever.Monitor)('ws_rsvps.js', {
+			max: 5,
+			silent: false,
+			args: ['--stream', args.stream, '--zk', host],
+			'spinSleepTime': 5000,
+		});
+
+		child.on('watch:restart', function(info) {
+		    console.error('Restaring script because ' + info.file + ' changed');
+		});
+
+		child.on('restart', function() {
+		    console.error('Forever restarting script for ' + child.times + ' time');
+		});
+
+		child.on('exit:code', function(code) {
+		    console.error('Forever detected script exited with code ' + code);
+		});
+	
+		child.start();
+	}
  
-  var child = new (forever.Monitor)('ws_rsvps.js', {
-    max: 5,
-    silent: false,
-    args: [args.stream, args.zk],
-    'spinSleepTime': 5000,
-  });
- 
-  child.on('exit', function () {
-    console.log('ws_rsvps.js has exited after 5 restarts');
-  });
- 
-  
-  setTimeout(function() {
-  	console.log ('Starting Websocket stream: ' + args.stream + ' zk-host: ' + args.zk);
-  }, 5000);
-  //child.start();
+	
+
+
+	
---
+++ tutum.js
@@ -0,0 +1,64 @@
+'use strict';
+
+var querystring = require('querystring'),
+    util = require('util');
+
+var request = require('superagent');
+
+var Tutum = function (configuration) {
+  var defaults = {
+    baseEndpoint: 'https://dashboard.tutum.co/api/v1',
+    username: '',
+    apiKey: ''
+  };
+
+  /*eslint-disable no-underscore-dangle*/
+  this.configuration = util._extend(defaults, configuration);
+  /*eslint-enable no-underscore-dangle*/
+  this.configuration.authorization = this.configuration.username + ':' + this.configuration.apiKey;
+
+  this.request = function (method, path, params, callback) {
+    var body = {};
+
+    var url = this.configuration.baseEndpoint;
+
+    // Add leading and trailing slashes to the path if they do not exist
+    url += path.replace(/^\/?/, '/').replace(/\/?$/, '/');
+
+    callback = (typeof params === 'function') ? params : callback;
+
+    method = method.toLowerCase();
+    method = (method === 'delete') ? 'del' : method;
+
+    if (method === ('post' || 'patch')) {
+      body = params;
+    } else {
+      url += '?' + querystring.stringify(params);
+    }
+    request
+      [method](url)
+      .send(body)
+      .set('Accept', 'application/json')
+      //.set('Authorization', 'ApiKey ' + this.configuration.authorization)
+      .set('Authorization', 'Basic ' + new Buffer(this.configuration.authorization).toString('base64'))     
+      .set('Content-Type', 'application/json')
+      .end(function (err, res) {
+        if (err) {
+          return callback(err);
+        }
+        if (res.error) {
+          return callback(res.error);
+        }
+        callback(null, res.body);
+      });
+  };
+};
+
+// Helper instance methods for get, post, patch and delete
+[ 'get', 'post', 'patch', 'delete' ].forEach(function (method) {
+  Tutum.prototype[method] = function (path, params, callback) {
+    this.request(method, path, params, callback);
+  };
+});
+
+module.exports = Tutum;
